0f36f9366d6819f7dcfb21bb0a68cf9cf85aa6ee
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-01-08 17:41:22 +0100

    Adapter\Zip test

---
+++ tests/Adapter/ZipTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WyriHaximus\Tests\FlyPie\Adapter;
+
+use WyriHaximus\FlyPie\Adapter\Zip;
+
+class ZipTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $this->assertInstanceOf('League\Flysystem\Adapter\Zip', (new Zip())->create([
+            'path' => sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'wyrihaximus_fly_pie_test_adapter.zip',
+        ]));
+    }
+}
