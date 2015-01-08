55f5153d4ce751cc5a844f7fb651ca971609e6d9
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-01-08 21:49:13 +0100

    Missing docblock

--- src/FilesystemsTrait.php
+++ src/FilesystemsTrait.php
@@ -4,6 +4,13 @@ namespace WyriHaximus\FlyPie;
 
 trait FilesystemsTrait
 {
+    /**
+     * Proxy to the FilesystemRegistry.
+     *
+     * @param string $alias Alias name requested.
+     *
+     * @return mixed
+     */
     public function filesystem($alias)
     {
         return FilesystemRegistry::get($alias);
