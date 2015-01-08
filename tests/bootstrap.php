a8d4e2ae91ef4003c53598844541568aedc6c1f6
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-01-08 17:38:02 +0100

    Cleaned up tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,11 +4,3 @@ require dirname(__DIR__) . '/vendor/cakephp/cakephp/src/basics.php';
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 define('PLUGIN_REPO_ROOT', dirname(__DIR__) . DS);
-
-Cake\Core\Plugin::load(
-	'TwigView',
-	[
-		'namespace' => 'WyriHaximus\\CakePHP\FlyPie',
-		'path' => PLUGIN_REPO_ROOT . 'src' . DS,
-	]
-);
