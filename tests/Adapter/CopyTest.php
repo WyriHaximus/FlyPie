1e500ea37a9bdc0d42126d6f85234b78d1e6dd82
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-01-08 21:25:39 +0100

    Adapter\Copy test

---
+++ tests/Adapter/CopyTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WyriHaximus\Tests\FlyPie\Adapter;
+
+use WyriHaximus\FlyPie\Adapter\Copy;
+
+class CopyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $this->assertInstanceOf('League\Flysystem\Adapter\Copy', (new Copy())->create([
+            'consumerKey' => '1',
+            'consumerSecret' => '2',
+            'accessToken' => '3',
+            'tokenSecret' => '4',
+        ]));
+    }
+}
