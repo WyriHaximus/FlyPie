0793d95eacc427fdb66839a23348154355643ea1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-02-21 20:53:15 +0100

    Controller to get filesystem contents from for debug bar

---
+++ src/Controller/FilesystemPanelController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace WyriHaximus\FlyPie\Controller;
+
+use Cake\Controller\Controller;
+use Cake\Core\Configure;
+use Cake\Event\Event;
+use Cake\Network\Exception\NotFoundException;
+use WyriHaximus\FlyPie\FilesystemsTrait;
+
+/**
+ * Class FilesystemPanelController
+ * @package WyriHaximus\FlyPie\Controller
+ */
+class FilesystemPanelController extends Controller
+{
+    use FilesystemsTrait;
+
+    /**
+     * Before filter handler.
+     *
+     * @param Event $event The event.
+     *
+     * @return void
+     *
+     * @throws NotFoundException Throw no found when debug is disabled.
+     */
+    public function beforeFilter(Event $event)
+    {
+        // TODO add config override.
+        if (!Configure::read('debug')) {
+            throw new NotFoundException();
+        }
+    }
+
+    /**
+     * List contents from directory $path on filesystem $filesystem.
+     *
+     * @param string $filesystem Filesystem to list contents in $path from.
+     * @param string $path       List contents from here.
+     *
+     * @return void
+     */
+    public function listContents($filesystem, $path = '')
+    {
+        $this->set([
+            '_serialize' => [
+                'directoryContents',
+            ],
+            'directoryContents' => $this->filesystem($filesystem)->listContents($path),
+        ]);
+    }
+}
