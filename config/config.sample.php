1c563baf097ee25b54643e2869f903a26ac6da7f
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-11-29 08:14:54 +0100

    Updated config example with extra documentation regarding adapter arguments and FQCN exaples instead of their `simple` names

--- config/config.sample.php
+++ config/config.sample.php
@@ -1,34 +1,58 @@
 <?php
 
+use League\Flysystem\Adapter\Ftp;
+use League\Flysystem\Adapter\Local;
+
 return [
     'WyriHaximus' => [
         'FlyPie' => [
             'thumbnails' => [
-                'adapter' => 'Local', // The adapter to use for this filesystem
+                'adapter' => Local::class,
 
+                /**
+                 * Array with data required to build the client, no need for it
+                 * when you're providing the client (adapter) or factory yourself.
+                 *This array is passed into the constructor using
+                 * ReflectionClass::newInstanceArgs So the first item in the array
+                 * is the first argument, the second item the second argument etc etc.
+                 * See the FTP example for adapters that require an associative array
+                 * with configuration as first argument.
+                 */
                 'vars' => [
-                    // Array with data required to build the client, no need for it when you're providing the client or factory yourself
+                    '/path/to/thumbnails/'
                 ],
 
-                // Optional, provide a set up client
+                // Optional, provide a set up client (adapter).
                 'client' => new stdClass(),
 
-                // Optional, provide a factory that builds the client
+                // Optional, provide a factory that builds the client (adapter).
                 'factory' => function() { // Build client via callback
                     return new stdClass();
                 },
 
                 // OR
 
-                'factory' => [$class, 'method'], // Array call user func
+                'factory' => [$class, 'method'], // Array call user func.
 
                 // OR
 
-                'factory' => 'Class::method', // String call user func, this is going to be interesting with the client from a event
+                'factory' => 'Class::method', // String call user func, this is going to be interesting with the client from a event.
 
                 // OR
 
-                'factory' => 'Vendor.Plugin.Event.Name', // Retrieve the client from an event
+                'factory' => 'Vendor.Plugin.Event.Name', // Retrieve the client (adapter) from the result property of an event.
+            ],
+
+            // FTP example
+            'some-ftp-server' => [
+                'adapter' => Ftp::class,
+                'vars' => [
+                    [
+                        'host' => 'example.com',
+                        'username' => 'username',
+                        'password' => 'password',
+                    ],
+                ],
             ],
         ],
     ],
