e3a6aebb2716deb752dbd8ecdb6b4931a95eba78
Matthias Wirtz <matthias.wirtz@hotmail.de>
2020-03-03 07:26:35 +0100

    fix the filesystem panel
    - move template (CakePHP 4)
    - AwsClient instances cannot be serialized, as we're getting the classname only let's do this in the panel directly
      - https://github.com/aws/aws-sdk-php/blob/master/src/AwsClient.php#L261

--- templates/element/filesystem_panel.php
+++ templates/element/filesystem_panel.php
@@ -1,17 +1,17 @@
 <?php
 
-use \WyriHaximus\FlyPie\Panel\FilesystemPanel;
+use WyriHaximus\FlyPie\Panel\FilesystemPanel;
 
 ?>
 
-<?php if (!empty($filesystems)): ?>
+<?php if (!empty($filesystems)) : ?>
     <h4>Configured filesystems</h4>
     <p class="warning">The following list used <strong><?= FilesystemPanel::CONFIGURE_KEY ?></strong> as filesystem list:</p>
     <ul class="neat-array depth-0">
-        <?php foreach ($filesystems as $filesystem): ?>
+        <?php foreach ($filesystems as $filesystem) : ?>
             <li class="expandable collapsed filesystemtree" data-alias="<?= $filesystem; ?>">
                 <strong><?= h($filesystem) ?></strong>
-                (<?= h(get_class($instances[$filesystem]->getAdapter())) ?>)
+                (<?= h($classes[$filesystem]) ?>)
                 <ul class="neat-array depth-1 loading" style="display: none;">
                     <li>Loading...</li>
                 </ul>
@@ -25,6 +25,6 @@ use \WyriHaximus\FlyPie\Panel\FilesystemPanel;
         });
     </script>
     <hr />
-<?php else: ?>
+<?php else : ?>
     <div class="warning"><?= __d('fly_pie', 'No configured filesystem found') ?></div>
 <?php endif ?>
